c7d840dd7f9415d6b1d7629cdc93a45c109d684d
MogKhang <97433944+MogKhang@users.noreply.github.com>
2026-03-29 17:51:10 +0700

    Refactor OCR function to use Google Cloud Vision
    Updated the OCR function parameters to include google_credentials and changed ocr_image_max_width to 960. Bypassed PaddleOCR checks and adjusted model directory handling.

--- CLI/videocr/api.py
+++ CLI/videocr/api.py
@@ -11,8 +11,8 @@ def save_subtitles_to_file(
         conf_threshold: int = 75, sim_threshold: int = 80, max_merge_gap_sec: float = 0.1, use_fullframe: bool = False,
         use_gpu: bool = False, use_angle_cls: bool = False, use_server_model: bool = False, brightness_threshold: int | None = None,
         ssim_threshold: int = 92, subtitle_position: str = "center", frames_to_skip: int = 1, crop_zones: list[dict[str, int]] | None = None,
-        ocr_image_max_width: int = 800, post_processing: bool = False, min_subtitle_duration_sec: float = 0.2,
-        normalize_to_simplified_chinese: bool = True, subtitle_alignments: list[str | None] | None = None) -> None:
+        ocr_image_max_width=960, post_processing=False, min_subtitle_duration_sec=0.2,
+        normalize_to_simplified_chinese=True, google_credentials="",):
 
     if crop_zones is None:
         crop_zones = []
@@ -22,21 +22,17 @@ def save_subtitles_to_file(
     elif len(subtitle_alignments) == 1:
         subtitle_alignments.append(None)
 
-    paddleocr_path = utils.find_paddleocr()
-    try:
-        utils.perform_hardware_check(paddleocr_path, use_gpu)
-    except SystemExit as e:
-        print(e, flush=True)
-        sys.exit(1)
+    # Bypassing PaddleOCR checks since we are using Google Cloud Vision
+    paddleocr_path = ""
+    det_model_dir, rec_model_dir, cls_model_dir = "", "", ""
 
-    det_model_dir, rec_model_dir, cls_model_dir = utils.resolve_model_dirs(lang, use_server_model)
-
-    v = Video(video_path, paddleocr_path, det_model_dir, rec_model_dir, cls_model_dir)
+    v = Video(video_path, paddleocr_path, det_model_dir, rec_model_dir, cls_model_dir, time_end)
     try:
         v.run_ocr(
             use_gpu, lang, use_angle_cls, time_start, time_end, conf_threshold,
             use_fullframe, brightness_threshold, ssim_threshold, subtitle_position,
-            frames_to_skip, crop_zones, ocr_image_max_width, normalize_to_simplified_chinese
+            frames_to_skip, crop_zones, ocr_image_max_width, normalize_to_simplified_chinese,
+            google_credentials
         )
     except Exception as e:
         print(f"Error: {e}", flush=True)
